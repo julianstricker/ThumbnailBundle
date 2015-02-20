6c7d5b3f185eb51cf37a7c90729d2e546b3bc792
Julian Stricker <julian.stricker@brandnamic.com>
2015-02-20 12:50:31 +0100

    Multiple changes

--- Controller/ThumbnailController.php
+++ Controller/ThumbnailController.php
@@ -5,42 +5,51 @@ namespace JuSt\ThumbnailBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
-class ThumbnailController extends Controller
-{
+class ThumbnailController extends Controller {
+
     public function thumbnailAction(Request $request) {
         ini_set("gd.jpeg_ignore_warning", 1);
         $img = $request->get('img', null);
         $maxx = $request->get('maxx', '');
         $maxy = $request->get('maxy', '');
-        $mode = $request->get('mode', 'normal');    
-        $placeholder = $request->get('placeholder', 'noimage.jpg');    
-        $uploadsdir=$this->container->getParameter('uploadsdir');
-        $imgname= $uploadsdir.$img;
-        if (!is_file($imgname) || !is_readable($imgname)){
-            $imgname=__DIR__.'/../../../../web/images/placeholder/'.$placeholder;
-            
+        $mode = $request->get('mode', 'normal');
+        $imagesrootdir =  $this->container->hasParameter('just_thumbnailbundle.imagesrootdir') ? $this->container->getParameter('just_thumbnailbundle.imagesrootdir') : $this->container->getParameter('kernel.root_dir') . '/../web/';
+        $placeholder = $this->container->hasParameter('just_thumbnailbundle.placeholder') ? $this->container->getParameter('just_thumbnailbundle.placeholder') : null;
+        $placeholder = $request->get('placeholder', $placeholder);
+        
+        $imgname = $imagesrootdir . ltrim($img,'/\\');
+        if (!is_file($imgname) || !is_readable($imgname)) {
+            if (is_null($placeholder)){
+                throw new Symfony\Component\HttpKernel\Exception\NotFoundHttpException("Image not found");
+            }
+            $imgname = $placeholder;
         }
         $info = getimagesize($imgname);
         $ctime = filectime($imgname);
-        if (!$info){
-            $imgname=__DIR__.'/../../../../web/images/placeholder/'.$placeholder;
+        if (!$info) {
+            if (is_null($placeholder)){
+                throw new Symfony\Component\HttpKernel\Exception\NotFoundHttpException("Image not found");
+            }
+            $imgname = $placeholder;
             $info = getimagesize($imgname);
             $ctime = filectime($imgname);
         }
-        $expires = 1*24*60*60;
+        $expires = 1 * 24 * 60 * 60;
         $cachename = md5($imgname . $maxx . $maxy . $mode . $ctime);
-        if ($cachefile = $this->get('cache')->fetch('JSThumbnailBundle' . $cachename)) {
+        if ($cachefile = $this->get('cache')->fetch('JuStThumbnailBundle' . $cachename)) {
             //ist bereits im cache:
-            $uscachefile=unserialize($cachefile);
+            $uscachefile = unserialize($cachefile);
             $response = new Response($uscachefile);
             $response->headers->set('Content-Type', 'image/jpeg');
             $response->headers->set('Content-Length', strlen($uscachefile));
             $response->headers->set('Pragma', 'public');
-            $response->headers->set('Last-Modified', gmdate( 'D, d M Y H:i:s', $ctime ) . ' GMT' ); 
-            $response->headers->set('Cache-Control', 'maxage='.$expires);
-            $response->headers->set('Expires', gmdate('D, d M Y H:i:s', time()+$expires) . ' GMT');
-            return $response;            
+            $response->headers->set('Last-Modified', gmdate('D, d M Y H:i:s', $ctime) . ' GMT');
+            $response->headers->set('Cache-Control', 'maxage=' . $expires);
+            $response->headers->set('Expires', gmdate('D, d M Y H:i:s', time() + $expires) . ' GMT');
+            return $response;
         } else {
             //thumbnail erstellen:      
             if ($info[2] == 1) { //Original ist ein GIF
@@ -53,29 +62,28 @@ class ThumbnailController extends Controller
                 $oimage = $this->imagecreatefrombmp($imgname);
             } else {
 //                print_r($info);
-                die();
+                throw new Symfony\Component\HttpKernel\Exception\HttpException(500, "Error reading image");
                 //throw $this->createNotFoundException('The image does not exist or is not readable');
             }
             $ogrx = $info[0];
             $ogry = $info[1];
-            
-            
-            if ($mode == 'max'){
-                $ngrx=$ogrx;
-                $ngry=$ogry;
-                if ($maxx!='' && $ngrx>$maxx){
-                    $ngrx=$maxx;
-                    $ngry=($ogry/$ogrx)*$maxx;
+
+
+            if ($mode == 'max') {
+                $ngrx = $ogrx;
+                $ngry = $ogry;
+                if ($maxx != '' && $ngrx > $maxx) {
+                    $ngrx = $maxx;
+                    $ngry = ($ogry / $ogrx) * $maxx;
                 }
-                if ($maxy!='' && $ngry>$maxy){
-                    $ngry=$maxy;
-                    $ngrx=($ogrx/$ogry)*$maxy;
+                if ($maxy != '' && $ngry > $maxy) {
+                    $ngry = $maxy;
+                    $ngrx = ($ogrx / $ogry) * $maxy;
                 }
-                
-            }else{
-                if ($maxx=='') $maxx = ($ogrx/$ogry)*$maxy;
-                if ($maxy=='') $maxy = ($ogry/$ogrx)*$maxx;
-                if ($mode == 'crop') {            
+            } else {
+                if ($maxx == '') $maxx = ($ogrx / $ogry) * $maxy;
+                if ($maxy == '') $maxy = ($ogry / $ogrx) * $maxx;
+                if ($mode == 'crop') {
                     if ($ogrx / $maxx > $ogry / $maxy) { //Breitformat
                         $ngry = $maxy;
                         $ngrx = ( $ogrx * $maxy ) / $ogry;
@@ -94,13 +102,13 @@ class ThumbnailController extends Controller
                 }
             }
             if ($info[2] == 2 || $info[2] == 3 || $info[0] == 6) { //PNG, JPG
-                if ($mode == 'normal' || $mode=='max') {
+                if ($mode == 'normal' || $mode == 'max') {
                     $image = imagecreatetruecolor($ngrx, $ngry);
                 } else {
                     $image = imagecreatetruecolor($maxx, $maxy);
                 }
             } else { //GIF
-                if ($mode == 'normal' || $mode=='max') {
+                if ($mode == 'normal' || $mode == 'max') {
                     $image = imagecreate($ngrx, $ngry);
                 } else {
                     $image = imagecreate($maxx, $maxy);
@@ -114,7 +122,7 @@ class ThumbnailController extends Controller
             } else if ($info[2] == 1) { //GIF
                 $farbe_body = imagecolorallocate($image, 255, 255, 255);
             }
-            if ($mode == 'normal' || $mode=='max') {
+            if ($mode == 'normal' || $mode == 'max') {
                 imagecopyresampled($image, $oimage, 0, 0, 0, 0, $ngrx, $ngry, $ogrx, $ogry);
             } else if ($mode == 'crop') {
                 imagecopyresampled($image, $oimage, -($ngrx - $maxx) / 2, -($ngry - $maxy) / 2, 0, 0, $ngrx, $ngry, $ogrx, $ogry);
@@ -124,138 +132,112 @@ class ThumbnailController extends Controller
 
 
             ob_start(); // start a new output buffer
-            
-            
+
+
             if ($info[2] == 1) { //Original ist ein GIF
-                imagegif($image, NULL); 
+                imagegif($image, NULL);
             } else if ($info[2] == 2) { //Original ist ein JPG
-                imageinterlace($image,1);
-                imagejpeg($image, NULL, 100);     
+                imageinterlace($image, 1);
+                imagejpeg($image, NULL, 100);
             } else if ($info[2] == 3) { //Original ist ein PNG
-                imagepng($image, NULL);     
+                imagepng($image, NULL);
             } else if ($info[2] == 6) { //Original ist ein BMP
-                imageinterlace($image,1);
-                imagejpeg($image, NULL, 100);     
+                imageinterlace($image, 1);
+                imagejpeg($image, NULL, 100);
             }
-            
+
             $ImageData = ob_get_contents();
             ob_end_clean(); // stop this output buffer
-            $this->get('cache')->save('JSThumbnailBundle' . $cachename, serialize($ImageData));
+            $this->get('cache')->save('JuStThumbnailBundle' . $cachename, serialize($ImageData));
 
             $response = new Response($ImageData);
-            
+
             if ($info[2] == 1) { //Original ist ein GIF
-                $response->headers->set('Content-Type', 'image/gif');        
+                $response->headers->set('Content-Type', 'image/gif');
             } else if ($info[2] == 2) { //Original ist ein JPG
-                $response->headers->set('Content-Type', 'image/jpeg');        
+                $response->headers->set('Content-Type', 'image/jpeg');
             } else if ($info[2] == 3) { //Original ist ein PNG
-                $response->headers->set('Content-Type', 'image/png');        
+                $response->headers->set('Content-Type', 'image/png');
             } else if ($info[2] == 6) { //Original ist ein BMP
-                $response->headers->set('Content-Type', 'image/jpeg');        
+                $response->headers->set('Content-Type', 'image/jpeg');
             }
-                
+
             $response->headers->set('Content-Length', strlen($ImageData));
             $response->headers->set('Pragma', 'public');
-            $response->headers->set('Last-Modified', gmdate( 'D, d M Y H:i:s', $ctime ) . ' GMT' ); 
-            $response->headers->set('Cache-Control', 'maxage='.$expires);
-            $response->headers->set('Expires', gmdate('D, d M Y H:i:s', time()+$expires) . ' GMT');
+            $response->headers->set('Last-Modified', gmdate('D, d M Y H:i:s', $ctime) . ' GMT');
+            $response->headers->set('Cache-Control', 'maxage=' . $expires);
+            $response->headers->set('Expires', gmdate('D, d M Y H:i:s', time() + $expires) . ' GMT');
 
             imagedestroy($image);
             imagedestroy($oimage);
             return $response;
         }
     }
-    
-    public function imagecreatefrombmp($p_sFile) 
-    { 
+
+    private function imagecreatefrombmp($p_sFile) {
         //    Load the image into a string 
-        $file    =    fopen($p_sFile,"rb"); 
-        $read    =    fread($file,10); 
-        while(!feof($file)&&($read<>"")) 
-            $read    .=    fread($file,1024); 
-        
-        $temp    =    unpack("H*",$read); 
-        $hex    =    $temp[1]; 
-        $header    =    substr($hex,0,108); 
-        
+        $file = fopen($p_sFile, "rb");
+        $read = fread($file, 10);
+        while (!feof($file) && ($read <> "")) $read .= fread($file, 1024);
+        $temp = unpack("H*", $read);
+        $hex = $temp[1];
+        $header = substr($hex, 0, 108);
         //    Process the header 
         //    Structure: http://www.fastgraph.com/help/bmp_header_format.html 
-        if (substr($header,0,4)=="424d") 
-        { 
+        if (substr($header, 0, 4) == "424d") {
             //    Cut it in parts of 2 bytes 
-            $header_parts    =    str_split($header,2); 
-            
+            $header_parts = str_split($header, 2);
             //    Get the width        4 bytes 
-            $width            =    hexdec($header_parts[19].$header_parts[18]); 
-            
+            $width = hexdec($header_parts[19] . $header_parts[18]);
             //    Get the height        4 bytes 
-            $height            =    hexdec($header_parts[23].$header_parts[22]); 
-            
+            $height = hexdec($header_parts[23] . $header_parts[22]);
             //    Unset the header params 
-            unset($header_parts); 
-        } 
-        
+            unset($header_parts);
+        }
         //    Define starting X and Y 
-        $x                =    0; 
-        $y                =    1; 
-        
+        $x = 0;
+        $y = 1;
         //    Create newimage 
-        $image            =    imagecreatetruecolor($width,$height); 
-        
+        $image = imagecreatetruecolor($width, $height);
         //    Grab the body from the image 
-        $body            =    substr($hex,108); 
-
+        $body = substr($hex, 108);
         //    Calculate if padding at the end-line is needed 
         //    Divided by two to keep overview. 
         //    1 byte = 2 HEX-chars 
-        $body_size        =    (strlen($body)/2); 
-        $header_size    =    ($width*$height); 
-
+        $body_size = (strlen($body) / 2);
+        $header_size = ($width * $height);
         //    Use end-line padding? Only when needed 
-        $usePadding        =    ($body_size>($header_size*3)+4); 
-        
+        $usePadding = ($body_size > ($header_size * 3) + 4);
         //    Using a for-loop with index-calculation instaid of str_split to avoid large memory consumption 
         //    Calculate the next DWORD-position in the body 
-        for ($i=0;$i<$body_size;$i+=3) 
-        { 
+        for ($i = 0; $i < $body_size; $i+=3) {
             //    Calculate line-ending and padding 
-            if ($x>=$width) 
-            { 
+            if ($x >= $width) {
                 //    If padding needed, ignore image-padding 
                 //    Shift i to the ending of the current 32-bit-block 
-                if ($usePadding) 
-                    $i    +=    $width%4; 
-                
+                if ($usePadding) $i += $width % 4;
                 //    Reset horizontal position 
-                $x    =    0; 
-                
+                $x = 0;
                 //    Raise the height-position (bottom-up) 
-                $y++; 
-                
+                $y++;
                 //    Reached the image-height? Break the for-loop 
-                if ($y>$height) 
-                    break; 
-            } 
-            
+                if ($y > $height) break;
+            }
             //    Calculation of the RGB-pixel (defined as BGR in image-data) 
             //    Define $i_pos as absolute position in the body 
-            $i_pos    =    $i*2; 
-            $r        =    hexdec($body[$i_pos+4].$body[$i_pos+5]); 
-            $g        =    hexdec($body[$i_pos+2].$body[$i_pos+3]); 
-            $b        =    hexdec($body[$i_pos].$body[$i_pos+1]); 
-            
+            $i_pos = $i * 2;
+            $r = hexdec($body[$i_pos + 4] . $body[$i_pos + 5]);
+            $g = hexdec($body[$i_pos + 2] . $body[$i_pos + 3]);
+            $b = hexdec($body[$i_pos] . $body[$i_pos + 1]);
             //    Calculate and draw the pixel 
-            $color    =    imagecolorallocate($image,$r,$g,$b); 
-            imagesetpixel($image,$x,$height-$y,$color); 
-            
+            $color = imagecolorallocate($image, $r, $g, $b);
+            imagesetpixel($image, $x, $height - $y, $color);
             //    Raise the horizontal position 
-            $x++; 
-        } 
-        
+            $x++;
+        }
         //    Unset the body / free the memory 
-        unset($body); 
-        
+        unset($body);
         //    Return image-object 
         return $image;
-    } 
+    }
 }
---
+++ README.md
@@ -0,0 +1,58 @@
+= JuSt/ThumbnailBundle - Symfony2 Bundle for on-the-fly Thumbnails Creation =
+
+== Overview ==
+
+The bundle creates Thumbnails on the fly using GD for the Symfony2 framework. It uses the Symfony cache system to cache the thumbnails.
+It creates a thumbnail of a image in the given size and stores it in cache for the next calls, until the image changes.
+
+== License ==
+
+This bundle is released under the [MIT license](Resources/meta/LICENSE)
+
+== Installation ==
+
+To install the plugin use `symfony plugin-install` command
+{{{
+
+symfony plugin-install http://plugins.symfony-project.com/jsTumbnailPlugin
+
+}}}
+
+== Using the plugin ==
+Enable one or more modules in your settings.yml  * jsThumbnail
+{{{
+
+all:
+  .settings:
+    enabled_modules:        [ default, jsThumbnail ]
+    
+}}}
+
+In your template call something like this:
+{{{
+
+&lt;?php echo thumbnail_tag(&#039;uploads/pictures/image.jpg&#039;,100, 80, &#039;crop&#039; array(&#039;style&#039; =&gt; &#039;border: 1px solid #ff0000&#039;)) ?&gt; 
+//&#039;/path/to/image.jpg&#039;,maximum width, maximum height, params
+
+}}}
+
+The Parameter &quot;mode&quot; can be &quot;normal&quot;, &quot;crop&quot; or &quot;stretch&quot;
+you can call the Thumbnail directly:
+www.yourhost.com/yourapp.php/jsThumbnail/thumbnail?img=uploads/pictures/image.jpg&amp;maxx=100&amp;maxy=80&amp;mode=crop
+
+{{{
+
+&lt;?php use_helper(&#039;Thumbnail&#039;) ?&gt;
+
+&lt;?php echo thumbnail_tag(&#039;uploads/pictures/offer/54fbcc52d9ec1af3decd50aeed9f5517.jpg&#039;,100, 80, &#039;stretch&#039; array(&#039;style&#039; =&gt; &#039;border: 1px solid #ff0000&#039;)) ?&gt;
+
+}}}
+
+The Plugin automatically checks if the Original image was modificated.
+To delete the cached thumbnails call:
+{{{
+
+symfony cc
+
+}}}
+
