c89659015d7c82b69503c8b005bbe6079d8696b3
Julian Stricker <julian.stricker@brandnamic.com>
2015-02-24 10:02:47 +0100

    Fixed Namespace

--- DependencyInjection/JustThumbnailExtension.php
+++ DependencyInjection/JustThumbnailExtension.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace JuSt\ThumbnailBundle\DependencyInjection;
+namespace Just\ThumbnailBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
