b8e0b1c3b108376b6e1cf716e515d5808012fb91
Julian Stricker <julian.stricker@brandnamic.com>
2021-09-15 12:24:57 +0200

    Removed logger from thumbnailAction

--- Controller/ThumbnailController.php
+++ Controller/ThumbnailController.php
@@ -19,10 +19,10 @@ class ThumbnailController extends Controller {
      * @return Response
      */
     public function thumbnailAction(Request $request) {
-        if (1 !== ini_get('gd.jpeg_ignore_warning')) {
+        /*if (1 !== ini_get('gd.jpeg_ignore_warning')) {
             $logger = $this->get('logger');
             $logger->warning('The JustThumbnailBundle needs to have gd.jpeg_ignore_warning set to "1". Please set gd.jpeg_ignore_warning to "1" in your php.ini, and restart your webserver.');
-        }
+        }*/
 
         $img = $request->get('img', null);
         $maxx = $request->get('maxx', '');
