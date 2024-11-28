81c815f2c6ba7a6c514ea3763d8f1f6cafaf84b3
julianstricker <julian@julianstricker.com>
2024-11-28 12:41:24 +0100

    Symfony 5.4 Update

--- Controller/ThumbnailController.php
+++ Controller/ThumbnailController.php
@@ -12,7 +12,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class ThumbnailController extends Controller {
+class ThumbnailController extends AbstractController {
 
     /**
      * @param Request $request
